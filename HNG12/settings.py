a6934a9557d0f0dcc8c90ee295f222b89cd8fdbf
kingsleyesisi <kingsleyesisi@outlook.com>
2025-01-30 17:27:00 +0100

    Comment out Django REST framework and CORS headers in settings

--- HNG12/settings.py
+++ HNG12/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'cors_headers'
+    # 'rest_framework',
+    # 'cors_headers'
     'HNGAPP'
 ]
 
