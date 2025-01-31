9b8a7d3d3c4e811789b102522754ab78c17b98c9
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2025-01-31 13:41:46 +0100

    Update views.py

--- HNGAPP/views.py
+++ HNGAPP/views.py
@@ -1,25 +1,11 @@
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.renderers import JSONRenderer
-from .models import Info
+from django.http import JsonResponse
 
 from datetime import datetime
 
-All_Info = Info.objects.all()
-
-class GetInfo(APIView):
-    renderer_classes = [JSONRenderer]
-
-    def get(self, request):
-        for info in All_Info:
-            email = info.email
-            github_url = info.github_url
-            date_time = datetime.now()
-            data = {
-                "email": email,
-                "current_datetime": date_time,
-                "github_url" : github_url
-                }
-            
-        return Response(data, status=status.HTTP_200_OK)
+def get(request):
+        data = {
+            "email": "kingsleyesisi@yahoo.com",
+            "current_datetime": datetime.now(),
+            "github_url" : "https://kingsleyesisi/HNG_API"
+            }
+        return JsonResponse(data)
