97ec07c69afeea7a6b068c295200ee8c04c9e82a
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2025-01-31 19:40:45 +0100

    Update views.py

--- HNGAPP/views.py
+++ HNGAPP/views.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def get(request):
         data = {
             "email": "kingsleyesisi@yahoo.com",
-            "current_datetime": datetime.now(),
+            "current_datetime": datetime.now().replace(microsecond=0).isoformat()+"Z,
             "github_url" : "https://kingsleyesisi/HNG_API"
             }
         return JsonResponse(data)
