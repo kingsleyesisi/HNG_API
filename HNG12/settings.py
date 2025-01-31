c2f04b367db972b3e97b9c7cc232df8646f1c067
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2025-01-31 21:17:59 +0100

    Update settings.py

--- HNG12/settings.py
+++ HNG12/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'rest_framework',
-    'corsheaders'
-    'HNGAPP'
+    'corsheaders',
+    'HNGAPP',
 ]
 
 CORS_ALLOW_ALL_ORIGIN = []
